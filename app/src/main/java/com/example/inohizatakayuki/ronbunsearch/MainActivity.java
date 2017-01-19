1adf42fefaef70f09f2a75d7096a5fba71ba530a
Takayuki Inohiza <takayuki_0711sosdx@yahoo.co.jp>
2017-01-19 22:35:33 +0900

    類義語(仮の物)を抽出できるように実装

--- app/src/main/java/com/example/inohizatakayuki/ronbunsearch/MainActivity.java
+++ app/src/main/java/com/example/inohizatakayuki/ronbunsearch/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -29,15 +30,15 @@ import edu.cmu.lti.jawjaw.pobj.POS;
 import static com.example.inohizatakayuki.ronbunsearch.R.id.button1;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    AutoCompleteTextView mAutoCompleteTextView;
     EditText editText;
     TextView textView;
     Button button;
     Button button2;
     ListView listView;
-    String[] list;
+    String[] string;
     List<String> dataList;
-    ArrayAdapter<String> adapter;
+    int itemNum=0;
+    ArrayAdapter<String> adapter, adapter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,16 +59,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //simpleDemo.run("sell", POS.v);
 
         dataList = new ArrayList<String>();
-        list = new String[] {
-                "alpha",
-                "bravo",
-                "charlie",
-                "delta",
-                "echo"
+        string = new String[]{
+                "catch","have","take","make","acquire","obtain","receive","capture"
         };
         listView = (ListView) findViewById(R.id.listView1);
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.select_dialog_item, dataList);
+        adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.select_dialog_item, string);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -121,12 +120,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent i = new Intent(Intent.ACTION_VIEW, uri);
             startActivity(i);
         }else if(v==button2){
-            adapter.add("aaa");
-            adapter.notifyDataSetChanged();
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() { // 0.5秒後に実行
+                @Override
+                public void run() {
+                    if(itemNum<string.length&&adapter2.getItem(itemNum)!=null) {
+                        adapter.add(adapter2.getItem(itemNum));
+                        itemNum++;
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }, 500);
+            if(itemNum>=string.length){
+                Toast.makeText(getApplicationContext(), "類義語はこれ以上抽出できません",
+                        Toast.LENGTH_LONG).show();
+            }
 
         }
 
     }
-
 
 }
